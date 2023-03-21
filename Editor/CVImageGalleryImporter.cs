f6f681e8436717451d7c4df6f4c54d704f68260f
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-03-21 15:03:47 -0700

    Update CVImageGalleryImporter.cs

--- Editor/CVImageGalleryImporter.cs
+++ Editor/CVImageGalleryImporter.cs
@@ -1,10 +1,10 @@
 using UnityEditor;
 using UnityEngine;
 
-[InitializeOnLoad]
+// [InitializeOnLoad]
 public class CVImageGalleryImporter : MonoBehaviour
 {
-    // [MenuItem("CV Image Gallery/Import Unity Package")]
+    [MenuItem("CV Image Gallery/Import Unity Package")]
     private static void ImportUnityPackage()
     {
         string packagePath = "Packages/com.cj-mills.cv-image-gallery/Extras/CVImageGallery.unitypackage";
