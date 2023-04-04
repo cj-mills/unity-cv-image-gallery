dec553a1886caf615c23abd04401b948da7f237f
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-03 18:50:43 -0700

    Create PackageInstaller.cs

---
+++ Editor/PackageInstaller.cs
@@ -0,0 +1,60 @@
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+
+public class PackageInstaller
+{
+    private static AddRequest addRequest;
+
+    [InitializeOnLoadMethod]
+    private static void InstallDependencies()
+    {
+        string packageUrl = "https://github.com/cj-mills/Unity-Media-Display.git";
+
+        if (!IsPackageInstalled("com.cj-mills.unity-media-display"))
+        {
+            addRequest = Client.Add(packageUrl);
+            EditorApplication.update += PackageInstallationProgress;
+        }
+    }
+
+    private static void PackageInstallationProgress()
+    {
+        if (addRequest.IsCompleted)
+        {
+            if (addRequest.Status == StatusCode.Success)
+            {
+                UnityEngine.Debug.Log($"Successfully installed: {addRequest.Result.packageId}");
+            }
+            else if (addRequest.Status >= StatusCode.Failure)
+            {
+                UnityEngine.Debug.LogError($"Failed to install package: {addRequest.Error.message}");
+            }
+
+            EditorApplication.update -= PackageInstallationProgress;
+        }
+    }
+
+    private static bool IsPackageInstalled(string packageName)
+    {
+        var listRequest = Client.List(true, false);
+        while (!listRequest.IsCompleted) { }
+
+        if (listRequest.Status == StatusCode.Success)
+        {
+            foreach (var package in listRequest.Result)
+            {
+                if (package.name == packageName)
+                {
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            UnityEngine.Debug.LogError($"Failed to list packages: {listRequest.Error.message}");
+        }
+
+        return false;
+    }
+}
