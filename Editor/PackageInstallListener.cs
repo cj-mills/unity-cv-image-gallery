45c819bb4f7b8c06bbca9f51a245b5b15134f10f
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-03 23:10:14 -0700

    Update PackageInstallListener.cs

--- Editor/PackageInstallListener.cs
+++ Editor/PackageInstallListener.cs
@@ -6,51 +6,51 @@ using UnityEngine;
 [InitializeOnLoad]
 public class PackageInstallListener
 {
-    private static ListRequest listRequest;
+    // private static ListRequest listRequest;
 
-    static PackageInstallListener()
-    {
-        EditorApplication.update += CheckPackageInstallation;
-    }
+    // static PackageInstallListener()
+    // {
+    //     EditorApplication.update += CheckPackageInstallation;
+    // }
 
-    private static void CheckPackageInstallation()
-    {
-        Debug.Log("Here");
+    // private static void CheckPackageInstallation()
+    // {
+    //     Debug.Log("Here");
 
-        if (listRequest == null)
-        {
-            listRequest = Client.List(true, true);
-            return;
-        }
+    //     if (listRequest == null)
+    //     {
+    //         listRequest = Client.List(true, true);
+    //         return;
+    //     }
 
-        if (!listRequest.IsCompleted)
-        {
-            return;
-        }
+    //     if (!listRequest.IsCompleted)
+    //     {
+    //         return;
+    //     }
 
-        if (listRequest.Status == StatusCode.Success)
-        {
-            foreach (var package in listRequest.Result)
-            {
-                if (package.name == "com.cj-mills.cv-image-gallery" && package.status == PackageStatus.Available)
-                {
-                    Debug.Log("Package installed. Running custom code...");
+    //     if (listRequest.Status == StatusCode.Success)
+    //     {
+    //         foreach (var package in listRequest.Result)
+    //         {
+    //             if (package.name == "com.cj-mills.cv-image-gallery" && package.status == PackageStatus.Available)
+    //             {
+    //                 Debug.Log("Package installed. Running custom code...");
 
-                    // Run your desired code here.
+    //                 // Run your desired code here.
 
-                    // Unregister the event to prevent it from running again.
-                    EditorApplication.update -= CheckPackageInstallation;
-                }
-            }
-        }
-        else if (listRequest.Status >= StatusCode.Failure)
-        {
-            Debug.LogError("Failed to check for package installation.");
-            EditorApplication.update -= CheckPackageInstallation;
-        }
+    //                 // Unregister the event to prevent it from running again.
+    //                 EditorApplication.update -= CheckPackageInstallation;
+    //             }
+    //         }
+    //     }
+    //     else if (listRequest.Status >= StatusCode.Failure)
+    //     {
+    //         Debug.LogError("Failed to check for package installation.");
+    //         EditorApplication.update -= CheckPackageInstallation;
+    //     }
 
-        listRequest = null;
+    //     listRequest = null;
 
-        EditorApplication.update -= CheckPackageInstallation;
-    }
+    //     EditorApplication.update -= CheckPackageInstallation;
+    // }
 }
