a1b0c1c596b7b5e870f7141763f9d10807b97608
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-10 12:39:43 -0700

    Update AddCustomDefineSymbol.cs

--- Editor/AddCustomDefineSymbol.cs
+++ Editor/AddCustomDefineSymbol.cs
@@ -1,24 +1,24 @@
-using UnityEditor;
-using UnityEngine;
+// using UnityEditor;
+// using UnityEngine;
 
-namespace CJM.BarracudaInference
-{
-    public class DependencyDefineSymbolAdder
-    {
-        private const string CustomDefineSymbol = "CJM_CV_IMAGE_GALLERY";
+// namespace CJM.BarracudaInference
+// {
+//     public class DependencyDefineSymbolAdder
+//     {
+//         private const string CustomDefineSymbol = "CJM_CV_IMAGE_GALLERY";
 
-        [InitializeOnLoadMethod]
-        public static void AddCustomDefineSymbol()
-        {
-            var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
-            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+//         [InitializeOnLoadMethod]
+//         public static void AddCustomDefineSymbol()
+//         {
+//             var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+//             var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
 
-            if (!defines.Contains(CustomDefineSymbol))
-            {
-                defines += $";{CustomDefineSymbol}";
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, defines);
-                Debug.Log($"Added custom define symbol '{CustomDefineSymbol}' to the project.");
-            }
-        }
-    }
-}
+//             if (!defines.Contains(CustomDefineSymbol))
+//             {
+//                 defines += $";{CustomDefineSymbol}";
+//                 PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, defines);
+//                 Debug.Log($"Added custom define symbol '{CustomDefineSymbol}' to the project.");
+//             }
+//         }
+//     }
+// }
